2d5b26715947eddf2cbb991968b47e6522c5ddc0
divipavankumar92 <147476470+divipavankumar92@users.noreply.github.com>
2025-09-06 00:18:03 +0530

    Catalogue

---
+++ cicd-terraform-master-agent/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "devopspavandivi.shop"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z062895025NJSXZ288C3O"
+  description = "description"
+}
