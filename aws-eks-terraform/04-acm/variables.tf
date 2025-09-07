3c5b2664a45d1455184926b466a67f7965f9cf16
divipavankumar92 <147476470+divipavankumar92@users.noreply.github.com>
2025-09-07 12:59:08 +0530

    adding files

---
+++ aws-eks-terraform/04-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z062895025NJSXZ288C3O"
+}
+
+variable "zone_name" {
+    default = "devopspavandivi.shop"
+}
