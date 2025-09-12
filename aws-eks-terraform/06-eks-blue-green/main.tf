3d22070e5d0370dcb557233dd6b07b1721e9355f
DiviPavanKumar <pavandivi.kumar47@gmail.com>
2025-09-12 22:49:43 +0530

    Adding Files

--- aws-eks-terraform/06-eks-blue-green/main.tf
+++ aws-eks-terraform/06-eks-blue-green/main.tf
@@ -33,10 +33,10 @@ module "eks" {
   eks_managed_node_groups = {
     blue = {
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
+      instance_types = ["t3.medium"] #For Practice Only. Not for Production.
 
       min_size     = 2
-      max_size     = 10
+      max_size     = 5
       desired_size = 2
 
       iam_role_additional_policies = {
